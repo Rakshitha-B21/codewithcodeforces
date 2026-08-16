dba7d07c9718c82153a5ba80f3be490c9f35a4b0
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-08-17 00:02:37 +0530

    200B

---
+++ 200B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    double ans = 0;
+    for(int i=1;i<=n;i++){
+        double x;
+        cin>>x; 
+
+        ans += x;
+    }
+    cout<<fixed<<setprecision(12)<<ans/n<<endl;
+    return 0;
+}
