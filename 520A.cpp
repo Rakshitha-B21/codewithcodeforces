04178b3d11e31a16bb802d15ca7b948b298ad408
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-08-16 19:03:25 +0530

    520A

---
+++ 520A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    set<int> st;
+
+    for(char c:s){
+        st.insert(tolower(c));
+    }
+
+    if(st.size() == 26){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+
+    
+
+}
