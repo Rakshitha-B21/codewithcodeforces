20faaddd16426a812d9c48a11b835884cdb734b0
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-08-17 15:58:20 +0530

    275

---
+++ 275A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;        
+
+int main(){
+    int a[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>a[i][j];
+        }
+    }
+int dx[] = {0,0,0,1,-1};
+int dy[] = {0,1,-1,0,0};
+
+}
