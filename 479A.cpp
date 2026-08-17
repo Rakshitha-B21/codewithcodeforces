63c4600adafa83add5afeb9062356a89bdf5537a
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-08-17 22:57:00 +0530

    266B

---
+++ 479A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;        
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int ans = max({a+b+c,
+    a*b*c,
+    (a+b)*c,
+    a*(b+c)
+});
+
+cout<<ans<<endl;
+}
