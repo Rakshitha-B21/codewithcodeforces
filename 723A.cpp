46235b76533790454ad0044d1a7f86813d11da7b
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-08-26 10:56:41 +0530

    done

---
+++ 723A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int ans = max({a,b,c})-min({a,b,c});
+
+    cout<<ans<<endl;
+    return 0;
+    
+}
