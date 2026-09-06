0041076515ac60b63ebe8bd3684c62a2647158ad
Rakshitha-B21 <jeevanurakshabm@gmail.com>
2026-09-06 22:15:59 +0530

    added

---
+++ 110A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;    
+    cin >> s;
+    int cnt = 0;
+    for(char c:s){
+        if(c == '4' || c == '7'){
+            cnt++;
+        }
+    }
+
+    if(cnt == 4 || cnt == 7){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
